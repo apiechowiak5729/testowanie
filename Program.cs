6750a65280dbdef5694f4e5593801ed81a52e27a
apiechowiak5729 <90420476+apiechowiak5729@users.noreply.github.com>
2024-04-15 13:57:47 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ConsoleApp2
+﻿namespace Sortowanie_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] tablica = new int[20];
+            Random A = new Random();
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = A.Next(1, 40);
+                Console.Write($"{tablica[i]}" + " ");
+            }
+
+            int n;
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                for (int j = 0; j < tablica.Length - 1; j++)
+                {
+                    if (tablica[j] > tablica[j + 1])
+                    {
+                        n = tablica[j + 1];
+                        tablica[j + 1] = tablica[j];
+                        tablica[j] = n;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("posortowane");
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Console.Write(tablica[i] + " ");
+            }
         }
     }
 }
